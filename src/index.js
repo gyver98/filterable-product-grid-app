553f8efcbb3e7238d50c3e47e0d2c016879f20aa
somdari <matthewchoi7@gmail.com>
2017-12-09 20:06:00 +1100

    adding modules, plugins

---
+++ src/index.js
@@ -0,0 +1,13 @@
+require('./styles.scss');
+
+class Car {
+  
+    manufacturer(car) {
+      document.write(`I have a ${car}`)
+    }
+  
+  }
+  
+  const bmw = new Car;
+  
+  bmw.manufacturer('bmw');
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+
+let config = {
+  entry: './src/index.js', // entry file
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: 'bundle.js' // output filename
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // files ending with .js
+        exclude: /node_modules/, // exclude the node_modules directory
+        loader: "babel-loader" // use this (babel-core) loader
+      },
+      {
+        test: /\.scss$/, // files ending with .scss
+        use: ExtractTextWebpackPlugin.extract({ // call our plugin with extract method
+          use: ['css-loader', 'sass-loader'], // use these loaders
+          fallback: 'style-loader' // fallback for any CSS not extracted
+        }) // end extract
+      }
+    ] // end rules
+  },
+  plugins: [
+    new ExtractTextWebpackPlugin('styles.css') // call the ExtractTextWebpackPlugin constructor and name our css file
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, './public'), // A directory or URL to serve HTML content from.
+    historyApiFallback: true, // fallback to /index.html for Single Page Applications.
+    inline: true, // inline mode (set to false to disable including client scripts (like livereload)
+    open: true // open default browser while launching
+  },
+  devtool: 'eval-source-map' // enable devtool for better debugging experience
+}
+
+module.exports = config;
